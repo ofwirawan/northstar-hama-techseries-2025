013fe31973c5306a14071ad4927a20dcad2c6352
Alan Sebastian <alansbm88@gmail.com>
2025-09-05 00:33:47 +0800

    translation-page

---
+++ frontend/src/alan-frontend-src/App-home.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Class Components
+// class ClassComponent extends React.Component {
+//   render() {
+//     return <h2>Class Component</h2>
+//   }
+// }
+
+// function App() {} // to create components OR
+
+
+// const Card = ({ title }) => { // in javascript instead of background-color for styles, u use backgroundColor
+//   return (
+//     <div className="card">
+//     <h2>{ title }</h2>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div className="card-container">
+//     {/* <h2>Functional Arrow Component</h2> */}
+//     <Card title="Star Wars" rating={5} isCool={true}/>
+//     <Card title="Avatar" />
+//     <Card title="The Lion King" />
+//     </div>
+//   )
+// } // use arrow components
+// props or properties are a way to pass data from one component to the other (arguments passed to a function)
+
+const Language = ({language}) => {
+  return (
+    <div className='card'>
+      <p>{ language }</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <i><h4 id='NorthStar'> NorthStar</h4></i>
+      <div className="heading">
+        <div className='buttons'><img src="src/assets/left.svg"></img></div>
+        <h1>Change Language</h1>
+      </div>
+      <div className='languages'>
+        <Language language="English"/>
+        <Language language="Bahasa Indonesia"/>
+        <Language language="Tagalog"/>
+        <Language language="Melayu"/>
+        <Language language="Hindi"/>
+        <Language language="Bangla"/>
+      </div>
+    </div>
+  )
+}
+
+
+export default App
