c845cde3c597b193a72304b3deb459166e97975c
Alan Sebastian <alansbm88@gmail.com>
2025-09-05 22:25:24 +0800

    translation-page

---
+++ frontend/src/pages/main-translation.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '/Users/lenov/OneDrive/Documents/GitHub/northstar-hama-techseries-2025/frontend/src/pages/Translation.css'
+import App from '/Users/lenov/OneDrive/Documents/GitHub/northstar-hama-techseries-2025/frontend/src/pages/App-translation'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
