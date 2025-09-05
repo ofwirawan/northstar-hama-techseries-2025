72ac9dbeb2c3c729a809755c39c23587911595a2
Alan Sebastian <alansbm88@gmail.com>
2025-09-05 23:18:51 +0800

    dynamic changes

---
+++ frontend/src/pages/main-change-language.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './Change-Language.css'
+import App from './App-change-language.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
